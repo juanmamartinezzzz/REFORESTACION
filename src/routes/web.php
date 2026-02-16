d485ab2dbbad0992887e689da947be8ba9c55cc2
Juanma <juanmamartinezalbert@gmail.com>
2026-02-16 11:41:09 +0100

    juanma vistas

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('index'); });
+Route::get('/especies', function () { return view('especies'); });
+Route::get('/contacto', function () { return view('contacto'); });
+Route::get('/perfil', function () { return view('perfil'); });
